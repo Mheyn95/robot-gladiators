8a1b6ba18af0f9703ab7708aa19f115559856470
mheyn95 <mheyn95@gmail.com>
2020-11-10 17:41:49 -0700

    add array of enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,15 +1,21 @@
+// Game States
+// "WIN" - Player robot has defeated all enemy-robots
+//  * Fight all enemy-robots
+//  * Defeat each enemy-robot
+// "Lose" - Player robot's health is zero or less
+
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-var enemyName = "Roberto";
+var enemyNames = ["Roberto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
 // this creates a function names "fight"
 
-var fight = function() {
+var fight = function(enemyName) {
     // Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
     // Fight prompt
@@ -63,4 +69,6 @@ var fight = function() {
     
 }
 
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
